0cd47390e009f63d666ecee3c08c07c94b746323
sasini <sasinidr@gmail.com>
2024-06-12 00:04:54 +0530

    Create Doctor web page

--- test1/settings.py
+++ test1/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'appointments',
-    'register',
+    'doctors',
 ]
 
 MIDDLEWARE = [
--- test1/urls.py
+++ test1/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('appointments.urls')),
+    path('', include('doctors.urls')),
 ]
